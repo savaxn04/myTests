91ee6804d74ecb9a2963df49d7ef7c2d71092c25
Dmytro Savchenko <dmitriysavchenko11@gmail.com>
2023-10-16 22:48:52 +0300

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = {
+  testDir: './tests',
+  timeout: 30*1000,
+  expect: {
+    timeout: 6000
+  },
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: 'chromium',
+    headless: false
+  }
+};
+
+module.exports = config;
+
